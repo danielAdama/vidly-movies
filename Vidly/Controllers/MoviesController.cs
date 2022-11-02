5c3ff31c0011ec68f95d834d73b50d10a7c5aa24
danielAdama <adamadaniel321@gmail.com>
2022-11-02 10:19:50 -0700

    Initial Commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
