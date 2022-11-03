139b659a8d74e58b058a09b213a9ff5ea31e79f5
danielAdama <adamadaniel321@gmail.com>
2022-11-02 20:38:27 -0700

    BaseEntity to inherit

---
+++ Vidly/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class BaseEntity
+    {
+        [Required]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
