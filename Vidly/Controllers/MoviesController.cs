16da2d90eb361b59a163dbffe04d0b505c0b8e01
danielAdama <adamadaniel321@gmail.com>
2022-11-02 18:57:20 -0700

    Fixes

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,5 +11,31 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek" };
             return View(movie);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id) 
+        {
+            return Content($"id= {id}");
+        }
+
+        [HttpGet]
+        public IActionResult Index(int? pageIndex, string sortBy) 
+        {
+            //if pageIndex is not specified we display the movies in page 1, likewise sortBy we sort by their names
+            if (!pageIndex.HasValue) 
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy)) 
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public IActionResult ByReleaseDate(int year, int month) 
+        {
+            return Content(year + "/" + month);
+        }
     }
 }
