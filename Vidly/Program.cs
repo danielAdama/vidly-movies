15dece9ce24eb0866d7b488da7a9e6ddb6fd05df
danielAdama <adamadaniel321@gmail.com>
2022-11-02 18:58:10 -0700

    Added routing for release date

--- Vidly/Program.cs
+++ Vidly/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 using Vidly.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -36,9 +37,26 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+//app.MapControllerRoute(
+//    name: "MoviesByReleaseDate",
+//    pattern: "movies/released/{year}/{month}",
+//    defaults: new { controller = "Movies", action = "ByReleaseDate" },
+//    //Use regex to allow only 4 digits for year and 2 for month
+//    constraints: new { year = @"\d{4}", month = @"\d{2}"}
+//    );
+
+app.MapControllerRoute(
+    name: "MoviesByReleaseDate",
+    pattern: "movies/released/{year}/{month}",
+    defaults: new { controller = "Movies", action = "ByReleaseDate" },
+    //Use regex to allow only 2020, 2021 & 2022 for year and 2 digits for month.
+    constraints: new { year = @"2020|2021|2022", month = @"\d{2}"}
+    );
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
