472b1152bd7d2418ac96f5dbf295ea5fe4c9d6af
danielAdama <adamadaniel321@gmail.com>
2022-11-03 18:02:12 -0700

    Add Random Movie View Model

---
+++ Vidly/ViewModel/RandomMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+using Vidly.Models;
+
+namespace Vidly.ViewModel
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customers> Customers { get; set; }
+    }
+}
