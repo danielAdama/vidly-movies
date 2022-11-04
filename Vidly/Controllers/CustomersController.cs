d6e1395ca8664333a106e8aec702f5d2d34e7188
danielAdama <adamadaniel321@gmail.com>
2022-11-03 19:31:13 -0700

    section 2 exercise

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+	public class CustomersController : Controller
+	{
+		public IActionResult Index()
+		{
+			var customers1 = new List<Customers>()
+			{
+				new Customers = { Id=1, Name = "John Smith" },
+				new Customers = { Id=2, Name = "Mary Williams" }
+			};
+			RandomMovieViewModel viewModel = new RandomMovieViewModel();
+			{
+				Customers = customers1,
+			};
+
+			return View(viewModel);
+		}
+	}
+}
